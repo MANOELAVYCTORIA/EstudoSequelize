0336c78e94baa99b50c32ded9f1fe326982180a7
manoelavyctoria <manuelavyctoria35@gmail.com>
2021-04-12 10:39:46 -0300

    Fazendo buscas no BD com siquelize

--- instagram.js
+++ instagram.js
@@ -1,31 +1,103 @@
 const {Usuario, Post, Comentario, sequelize} = require('./models');
+const {Op} = require('sequelize');
 
 
-Usuario.findAll()
-.then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
+// Usuario.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
 
-Post.findAll()
-.then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
+// Usuario.findAll({
+//     order: [
+//         ['nome', 'ASC'] //DESC => DECRECENTE
+//     ]
+// })
+// .then((resultado) => {
+//     console.log(resultado.map(Usuario => Usuario.toJSON()));
+// });
 
-Comentario.findAll()
-.then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
 
-Post.findOne({
+// Usuario.findAll({
+//     order: [
+//         ['id', 'ASC'] //DESC => DECRECENTE
+//     ],
+//     limit: 2,
+//     offset: 2
+// })
+// .then((resultado) => {
+//     console.log(resultado.map(Usuario => Usuario.toJSON()));
+// });
+
+// Post.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+// Post.findAll({
+//     where: {
+//         texto: {[Op.like]: '%oi%'}
+//     }
+// })
+// .then((resultado) => {
+//    console.log(resultado.map(post => post.toJSON()));
+// })
+
+
+// Comentario.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+// Post.findOne({
+//     where: {
+//         id: '2'
+//     }
+// })
+// .then((resultado) => {
+//     console.log(resultado)
+// });
+
+// Comentario.findByPk(3)
+// .then((resultado) => {
+//     console.log(resultado)
+// })
+
+/* SEQUELIZE - Consultas
+Buscar todos os usuarios que tem a letra A no nome
+Buscar todos os usuarios que não tem a letra A no nome
+Buscar todos os posts e exibir 2 por vez
+Utilize: Where, Op.like, Op.notLike, Limit, Offset, Order */
+
+console.log('A) Buscar todos os usuarios que tem a letra A no nome:')
+
+Usuario.findAll({
     where: {
-        id: '2'
+        nome: {[Op.like]: '%a%'}
     }
 })
 .then((resultado) => {
-    console.log(resultado)
+   console.log(resultado.map(Usuario => Usuario.toJSON()));
 });
 
-Comentario.findByPk(3)
-.then((resultado) => {
-    console.log(resultado)
+console.log('B) Buscar todos os usuarios que não tem a letra A no nome: ')
+
+Usuario.findAll({
+    where: {
+        nome: {[Op.notLike]: '%a%'}
+    }
 })
+.then((resultado) => {
+   console.log(resultado.map(Usuario => Usuario.toJSON()));
+});
+
+console.log('C) Buscar todos os posts e exibir 2 por vez: ')
+
+Comentario.findAll({
+    order: [
+        ['id', 'ASC'] //DESC => DECRECENTE
+    ],
+    limit: 2,
+})
+.then((resultado) => {
+    console.log(resultado.map(Comentario => Comentario.toJSON()));
+});
