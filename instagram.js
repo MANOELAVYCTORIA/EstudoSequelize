8372744f8b5d0d52e61dfc22d6cb37e859a0c8aa
manoelavyctoria <manuelavyctoria35@gmail.com>
2021-04-12 12:07:54 -0300

    Utilizando CREATE, UPDATE E DESTROY no siquelize

--- instagram.js
+++ instagram.js
@@ -68,36 +68,139 @@ Buscar todos os usuarios que não tem a letra A no nome
 Buscar todos os posts e exibir 2 por vez
 Utilize: Where, Op.like, Op.notLike, Limit, Offset, Order */
 
-console.log('A) Buscar todos os usuarios que tem a letra A no nome:')
+// console.log('A) Buscar todos os usuarios que tem a letra A no nome:')
 
-Usuario.findAll({
-    where: {
-        nome: {[Op.like]: '%a%'}
-    }
+// Usuario.findAll({
+//     where: {
+//         nome: {[Op.like]: '%a%'}
+//     }
+// })
+// .then((resultado) => {
+//    console.table(resultado.map(Usuario => Usuario.toJSON()));
+// });
+
+// console.log('B) Buscar todos os usuarios que não tem a letra A no nome: ')
+
+// Usuario.findAll({
+//     where: {
+//         nome: {[Op.notLike]: '%a%'}
+//     }
+// })
+// .then((resultado) => {
+//    console.table(resultado.map(Usuario => Usuario.toJSON()));
+// });
+
+// console.log('C) Buscar todos os comentarios e exibir 2 por vez: ')
+
+
+// for (let i = 0; i < 6; i += 2) {
+//     Comentario.findAll({
+//       order: [['id', 'DESC']],
+//       offset: i,
+//       limit: 2,
+//     }).then((resultado) => {
+//       console.table(resultado.map((comment) => comment.toJSON()));
+//     });
+//   }
+
+//   console.log('------------')
+
+//   Usuario.create ({
+//       nome: 'Manoela',
+//       email: 'manuela@gmail.com',
+//       senha: 'manu1234'
+//   }).then((resultado) => {
+//       console.log(resultado.toJSON());
+//   });
+
+//   Usuario.update({
+//       senha: 'novasenha'
+//   }, {
+//       where: {
+//           id: 4
+//       }
+//   }).then((resultado) => {
+//       console.log(resultado)
+//   })
+
+//   Usuario.destroy({
+//       where: {
+//           id: 7
+//       }
+//   }).then((resultado) => {
+//       console.log(resultado.toJSON())
+//   });
+
+/* SEQUELIZE - CRUD
+adicione todos os integrantes do seu grupo como Usuarios utilizando o método create
+crie um post relacionado ao seu usuario
+atualize o email do usuario 'Sergio' para 'sergio@digitalhouse.com'
+apague o usuario 'Felipe Veronesi' filtrando pelo id */
+
+console.log('------------------------------------------------------')
+
+console.log('A) adicione todos os integrantes do seu grupo como Usuarios utilizando o método create:');
+
+
+// Usuario.bulkCreate ([
+//     // {nome: 'Barbara Lícia', email: 'barbara@gmail.com', senha: 'barbara1234'},
+//     // {nome: 'Maria Luiza', email: 'maria@gmail.com', senha: 'maria1234'}
+//     // {nome: 'Lucio araujo', email: 'lucio@gmail.com', senha: 'lucio1234' },
+//     // {nome: 'Dari Diniz', email: 'dari@gmail.com', senha: 'dari1234'},
+//     // {nome: 'Angelo victor', email: 'angelo@gmail.com', senha: 'angelo1234'}
+
+// ])
+
+Usuario.findAll().then((usuarios) => {
+    console.table(usuarios.map((usuario) => usuario.toJSON()));
 })
+
+console.log('------------------------------------------------------')
+
+console.log('B) crie um post relacionado ao seu usuario: ')
+
+// Post.create ({
+//           texto: 'Aula da DH',
+//           img: ' ',
+//           usuarios_id: 5,
+//           n_likes: 0
+
+//       }).then((resultado) => {
+//           console.log(resultado.toJSON());
+//       });
+      
+      Post.findAll().then((resultado) => {
+           console.table(resultado.map(post => post.toJSON()));
+         });
+
+ console.log('------------------------------------------------------');
+
+ console.log('C) atualize o email do usuario Sergio para sergio@digitalhouse.com');
+
+//  Usuario.update({
+//           email: 'sergio@digitalhouse.com'
+//       }, {
+//           where: {
+//               id: 2
+//           }
+//       });
+
+Usuario.findAll()
 .then((resultado) => {
-   console.log(resultado.map(Usuario => Usuario.toJSON()));
+    console.table(resultado.map(usuario => usuario.toJSON()));
 });
 
-console.log('B) Buscar todos os usuarios que não tem a letra A no nome: ')
+console.log('------------------------------------------------------');
 
-Usuario.findAll({
-    where: {
-        nome: {[Op.notLike]: '%a%'}
-    }
-})
-.then((resultado) => {
-   console.log(resultado.map(Usuario => Usuario.toJSON()));
-});
+console.log('D) apague o usuario Felipe Veronesi filtrando pelo id');
 
-console.log('C) Buscar todos os posts e exibir 2 por vez: ')
+// Usuario.destroy({
+//           where: {
+//               id: 3
+//           }
+//       });
 
-Comentario.findAll({
-    order: [
-        ['id', 'ASC'] //DESC => DECRECENTE
-    ],
-    limit: 2,
-})
-.then((resultado) => {
-    console.log(resultado.map(Comentario => Comentario.toJSON()));
-});
+      Usuario.findAll()
+      .then((resultado) => {
+          console.table(resultado.map(usuario => usuario.toJSON()));
+      });
